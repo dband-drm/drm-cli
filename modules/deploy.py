bcdfb2a28d0ef975d9d9367c3dfff60a632e337e
eli shohat <elimirishohat@gmail.com>
2024-05-22 17:55:57 +0300

    Support running dryrun script (deploy)

--- modules/deploy.py
+++ modules/deploy.py
@@ -27,38 +27,62 @@ class MsSql:
     @drm_logger.log_decorator(logger) 
     def __init__(self, deploy_config): 
 
-        # Get SqlPackage from configuration
+        def get_location (self, app_name):
+            if which(app_name) != None:
+                return app_name
+            app = "{app_name}.exe".format(app_name = app_name)
+            if which(app) != None:
+                return app
+            # Not known --> search
+            f = files_and_folders.Files(app_name)
+            file_name = f.find_file_in_dir("/")
+            if (file_name == None):
+                f = files_and_folders.Files("{app_name}.exe".format(app_name = app_name))
+                file_name = f.find_file_in_dir("/")
+            if (file_name == None):
+                raise ('{file_name} utility not found!!!'.format(file_name = file_name))
+            else:
+                return (file_name.replace("\\", "\\\\"))
+
+
+        js = deploy_config.full_config
+        missing_object = False
+
+        #============================
+        # Identify SqlPackage utility
+        #============================
+        app_name = "sqlpackage"
         if hasattr(deploy_config, 'sqlpackage_path'):
+            # Get utility location from configuration
             self.upgrade_tool_file_name = deploy_config.sqlpackage_path
         else:
-            self.logger.info("sqlpackage utility path was not supplied in drm_deploy.config, searching (This may take a while)...")
-            # Known from PATH or in current directory
-            app = "sqlpackage"
-            if which(app) != None:
-                self.upgrade_tool_file_name = app
-            app = "sqlpackage.exe"
-            if which(app) != None:
-                self.upgrade_tool_file_name = app
-            # Not known --> search
-            if (which("sqlpackage") == None and which("sqlpackage.exe") == None):
-                f = files_and_folders.Files("sqlpackage")
-                self.upgrade_tool_file_name = f.find_file_in_dir("/")
-                if (self.upgrade_tool_file_name == None):
-                    f = files_and_folders.Files("sqlpackage.exe")
-                    self.upgrade_tool_file_name = f.find_file_in_dir("/")
-                if (self.upgrade_tool_file_name == None):
-                    raise ('sqlpackage utility not found!!!')
-                else:
-                    self.upgrade_tool_file_name = (self.upgrade_tool_file_name.replace("\\", "\\\\"))
-                    
-            self.logger.info("sqlpackage utility found & configured in drm_deploy.config for next deployments!!!")
+            # Utility not configured --> serach
+            missing_object = True
+            self.logger.info("{app_name} utility path was not supplied in drm_deploy.config, searching (This may take a while)...".format(app_name = app_name))
+            self.upgrade_tool_file_name = get_location (self, app_name)                    
+            self.logger.info("{app_name} utility found & configured in drm_deploy.config for next deployments!!!".format(app_name = app_name))
             locations_js = json.loads('{"sqlpackage_path": "' + self.upgrade_tool_file_name + '"}')
-            js = deploy_config.full_config
             js['locations'].append(locations_js)
+
+        app_name = "sqlcmd"
+        if hasattr(deploy_config, 'sqlcmd_path'):
+            # Get utility location from configuration
+            self.run_script_tool_file_name = deploy_config.sqlcmd_path
+        else:
+            # Utility not configured --> serach
+            missing_object = True
+            self.logger.info("{app_name} utility path was not supplied in drm_deploy.config, searching (This may take a while)...".format(app_name = app_name))
+            self.run_script_tool_file_name = get_location (self, app_name)                    
+            self.logger.info("{app_name} utility found & configured in drm_deploy.config for next deployments!!!".format(app_name = app_name))
+            locations_js = json.loads('{"sqlcmd_path": "' + self.run_script_tool_file_name + '"}')
+            js['locations'].append(locations_js)
+
+        if(missing_object):
             json_obj = json.dumps(js, indent=4)
             drm_config_file = os.path.join(current_working_directory, DEPLOY_CONFIG_FILE_NAME)
             file = files_and_folders.Files(drm_config_file)
             file.write_file(json_obj)
+
                 
 
     @drm_logger.log_decorator(logger) 
@@ -98,17 +122,16 @@ class MsSql:
         """ 
         return connection_string + "Database={project_targets_compare_db};".format(project_targets_compare_db = project_targets_compare_db)
                
-
     @drm_logger.log_decorator(logger) 
-    def generate_upgrade_script(self, solution_id, project_id, project_name, project_targets_compare_db, source_file, target_connection_string):
+    def generate_upgrade_script(self, solution_id, project_id, project_name, project_targets_compare_db, source_file, connection_string):
         """ 
         returns the fixed connection string using compare DB
         :param solution_id: solution ID
         :param project_id: project ID
         :param project_name: project name
-        :param project_targets_compare_db: target databasename to compare with
+        :param project_targets_compare_db: target database name to compare with
         :param source_file: source file name (dacpac)
-        :param target_connection_string: connectionstring
+        :param connection_string: connection string
         :return: update script name (String)
         """ 
         # Define upgrade script file
@@ -118,7 +141,7 @@ class MsSql:
         #=============================
         # Generate upgrade script file
         #=============================
-        result = subprocess.run([self.upgrade_tool_file_name, "/Action:script", "/SourceFile:" + source_file, "/TargetConnectionString:" + target_connection_string, "/OutputPath:" + upgrade_script], capture_output=True)
+        result = subprocess.run([self.upgrade_tool_file_name, "/Action:script", "/SourceFile:" + source_file, "/TargetConnectionString:" + connection_string, "/OutputPath:" + upgrade_script], capture_output=True)
         # Check if process exit with a failure
         if result.stderr:
             raise Exception (result.stderr)
@@ -130,6 +153,56 @@ class MsSql:
         self.logger.info("Upgrade script generated successfully!!!")
 
         return upgrade_script
+               
+    @drm_logger.log_decorator(logger) 
+    def run_upgrade_script(self, solution_id, project_id, project_name, target_name, upgrade_script, connection_string):
+        """ 
+        returns the fixed connection string using compare DB
+        :param solution_id: solution ID
+        :param project_id: project ID
+        :param project_name: project name
+        :param target_name: target database name
+        :param upgrade_script: upgrade script name
+        :param connection_string: connection string
+        :return: update script name (String)
+        """ 
+        upgrade_log_file = os.path.join (current_working_directory, "log", "S" + str(solution_id) + "-P" + str(project_id) + "-" + project_name + "-" + target_name + ".log")
+        self.logger.info("Running upgrade script against ""{target_name}"" (log file: ""{upgrade_log_file}"")...".format(target_name = target_name, upgrade_log_file = upgrade_log_file))
+        
+        # Exctract connection details
+        connection_win_auth = False
+        for param in connection_string.split(";"):
+            if (param.find("=") != -1):
+                [param_name, param_value] = param.split("=")
+                if (param_name.lower() in ['server']):
+                    connection_server = param_value
+                elif (param_name.lower() in ['user id', 'uid']):
+                    connection_username = param_value
+                elif (param_name.lower() in ['password', 'pwd']):
+                    connection_password = param_value
+                elif (param_name.lower() in ['trusted_connection', 'integrated security']) and (param_value.lower() in ['yes', 'true', 'sspi']):
+                    connection_win_auth = True
+
+        #==========================================
+        # Run upgrade script file against target DB
+        #==========================================
+        if not (connection_win_auth):
+            result = subprocess.run([self.run_script_tool_file_name, "-S", connection_server, "-U", connection_username, "-P", connection_password, "-i", upgrade_script, "-o", upgrade_log_file], capture_output=True)
+        else:
+            result = subprocess.run([self.run_script_tool_file_name, "-S", connection_server, "-E", "-i", upgrade_script, "-o", upgrade_log_file], capture_output=True)
+
+        # Check if process exit with a failure
+        if result.stderr:
+            raise Exception (result.stderr)
+            #raise subprocess.CalledProcessError(
+            #        returncode = result.returncode,
+            #        cmd = result.args,
+            #        stderr = result.stderr
+            #        )
+        self.logger.info("Upgrade finished successfully!!!")
+
+        return upgrade_script
+
 
 class Deploy:
 
@@ -241,5 +314,5 @@ class Deploy:
                             # Get list targets
                             targets_list_js = solution_obj.get_list_of_targets(project_targets_type_id, project_targets_list, project_targets_sql_text)
                             for target_db in targets_list_js:
-                                print(target_db)
+                                solution_obj.run_upgrade_script(solution_id, project_id, project_name, target_db, upgrade_script, target_connection_string)
                             
